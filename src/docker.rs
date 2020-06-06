09896b992913ff0cab39c959fc4cc304420e4f5d
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-06-07 02:53:29 +0430

    feat: show docker logs

--- src/docker.rs
+++ src/docker.rs
@@ -2,14 +2,23 @@ use std::path::Path;
 use std::process::Command;
 
 pub fn gen(root: &Path, dockerfile: String, args: String) -> String {
+    let params = format!(
+        "-f {} {}",
+        root.join(dockerfile.clone()).to_str().unwrap().to_string(),
+        root.to_str().unwrap().to_string()
+    );
+    Command::new("sh")
+        .arg("-c")
+        .arg(format!("sudo docker build {}", params))
+        .spawn()
+        .expect("failed to build docker image");
+
     std::str::from_utf8(
         &Command::new("sh")
             .arg("-c")
             .arg(format!(
-                "sudo docker run --rm $(sudo docker build -q -f {} {}) {}",
-                root.join(dockerfile).to_str().unwrap().to_string(),
-                root.to_str().unwrap().to_string(),
-                args
+                "sudo docker run --rm $(sudo docker build -q {}) {}",
+                params, args
             ))
             .output()
             .expect("failed to execute process")
